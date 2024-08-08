220f65cc6ce2466a0239a6a5815a42e1cd0a83ca
yu-no-m1i <snowapple.2525@gmail.com>
2024-08-08 15:05:06 +0900

    パスワード英数字で登録

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.last_name }
     email { Faker::Internet.email }
-    password { Faker::Internet.password(min_length: 6, mix_case: true, special_characters: false) }
+    password              {'aaaaa1'}
     password_confirmation { password }
     family_name { "山田" }
     first_name { "太郎" }
